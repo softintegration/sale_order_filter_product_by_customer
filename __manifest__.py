2b6dd1a5eff99ea4aa2a85d129b5262c4ee37aaa
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-11-27 22:49:33 +0300

    [FIX] Sale order:Allow the selection of product of type «Service»

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Sale order filter product by customer',
-    'version': '1.0.1',
+    'version': '1.0.2',
     'author':'Soft-integration',
     'category': 'Sale',
     'summary': '',
